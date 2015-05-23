e57468a0ba78fb45a01ed3042c5322b2aaa7bc4f
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-23 23:55:14 +0200

    Only remove when value is `null`, `undefined` or an empty string

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (obj) {
 	var o = objectAssign({}, obj);
 
 	Object.keys(o).forEach(function (key) {
-		if (!o[key]) {
+		if (o[key] === undefined || o[key] === null || o[key] === '') {
 			delete o[key];
 		}
 	});
