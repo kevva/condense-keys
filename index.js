c28ce26efe537ea017bf15ef85d4b01eadc2ebee
Kevin Martensson <kevinmartensson@gmail.com>
2015-09-03 09:37:19 +0200

    Make it faster

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var isObj = require('is-obj');
 var objectAssign = require('object-assign');
 
 function del(obj) {
-	Object.keys(obj).forEach(function (key) {
-		if (obj[key] === undefined || obj[key] === null || obj[key] === '') {
-			delete obj[key];
+	var keys = Object.keys(obj);
+
+	for (var i = 0; i < keys.length; i++) {
+		if (obj[keys[i]] === undefined || obj[keys[i]] === null || obj[keys[i]] === '') {
+			delete obj[keys[i]];
 		}
 
-		if (isObj(obj[key])) {
-			del(obj[key]);
+		if (isObj(obj[keys[i]])) {
+			del(obj[keys[i]]);
 		}
-	});
+	}
 
 	return obj;
 }
